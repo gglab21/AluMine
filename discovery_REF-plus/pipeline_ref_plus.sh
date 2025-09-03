1edccad8de092cdfebac90f9c58bea7547fba4b8
gglab21 <79469190+gglab21@users.noreply.github.com>
2025-09-03 14:23:59 -0400

    Update pipeline_ref_plus.sh
    make blast

--- discovery_REF-plus/pipeline_ref_plus.sh
+++ discovery_REF-plus/pipeline_ref_plus.sh
@@ -60,6 +60,7 @@ echo -n "Number of REF-plus candidates with TSD sequence: "
 cat Alu.pre-blast.kmer.db | wc -l
 
 # Run BLAST detected candidates and keep only the candidates that have >100 bits of homology with known Alu elements
+makeblastdb -in Alu.fas -dbtype nucl -out Alu
 blastn -query Alu.pre-blast.fas -db Alu -word_size 11 -gapopen 1 -gapextend 1 -dust no -num_alignments 1 -outfmt 6 | awk '{if($12>100)print}' | cut -f 1 | sort -n | uniq > Alu.REF-plus.blast
 ./extract_kmers_by_name.pl Alu.REF-plus.blast Alu.pre-blast.kmer.db > Alu.after-blast.kmer.db
 ./extract_fasta_by_name.pl Alu.REF-plus.blast Alu.pre-blast.fas > Alu.REF-plus.fas
