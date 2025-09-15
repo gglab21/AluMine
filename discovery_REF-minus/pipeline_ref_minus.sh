bbed99edf5ed09f7bb105d4ba833379175256e8c
gglab21 <79469190+gglab21@users.noreply.github.com>
2025-09-15 19:55:19 -0400

    Update pipeline_ref_minus.sh

--- discovery_REF-minus/pipeline_ref_minus.sh
+++ discovery_REF-minus/pipeline_ref_minus.sh
@@ -82,7 +82,7 @@ if [ ! -s $gtester_index ] || [ ! -s $gtester_names ];then
 "${human_chr_path}/chr21.fa" \
 "${human_chr_path}/chr22.fa" \
 "${human_chr_path}/chrX.fa" \
-"${human_chr_path}/chrY.fa" \
+"${human_chr_path}/chrY.fa" 
 #"${human_chr_path}/chrMT.fa"
 fi
 
